ea04b3ef243b21b155d7f04d63573704b8c2ed1c
anurag <sibuftp@gmail.com>
2021-11-07 21:28:39 +0530

    adding new models in core models

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
-from core.models import Contact
+from core.models import Student,Teacher,Book,BookIssue,Event,Due,Contact
 
 # Register your models here.
 
+admin.site.register(Student)
+admin.site.register(Teacher)
+admin.site.register(Book)
+admin.site.register(BookIssue)
+admin.site.register(Event)
+admin.site.register(Due)
 admin.site.register(Contact)
--- core/models.py
+++ core/models.py
@@ -5,7 +5,110 @@ from django.contrib.auth import get_user_model
 
 # Create your models here.
 
+RETURN_CHOICES = [
+    ('Yes', 'Yes'),
+    ('No', 'No'),
+]
+
 User = get_user_model()
+
+class Student(models.Model):
+	name=models.CharField(max_length=100)
+	department=models.CharField(max_length=100)
+	user=models.OneToOneField(User,related_name='student_reg',on_delete=models.CASCADE,null=True)
+	created_on=models.DateTimeField(auto_now_add=True)
+	updated_on=models.DateTimeField(auto_now=True)
+
+	class Meta:
+		ordering=['-created_on']
+
+	def save(self, *args, **kwargs):
+		user = get_current_user()
+		if user and not user.pk:
+			user = None
+		if not self.pk:
+			self.user = user
+
+		super(Student, self).save(*args, **kwargs)
+
+class Teacher(models.Model):
+	name=models.CharField(max_length=100)
+	user=models.OneToOneField(User,related_name='teacher_reg',on_delete=models.CASCADE,null=True)
+	created_on=models.DateTimeField(auto_now_add=True)
+	updated_on=models.DateTimeField(auto_now=True)
+
+	class Meta:
+		ordering=['-created_on']
+
+	def __str__(self):
+		return str(self.name)
+
+	def save(self, *args, **kwargs):
+		user = get_current_user()
+		if user and not user.pk:
+			user = None
+		if not self.pk:
+			self.user = user
+
+		super(Teacher, self).save(*args, **kwargs)
+
+
+class Book(models.Model):
+	name=models.CharField(max_length=150)
+	author=models.CharField(max_length=100)
+	publication=models.CharField(max_length=200)
+	publication_year=models.DateTimeField()
+	created_on=models.DateTimeField(auto_now_add=True)
+	updated_on=models.DateTimeField(auto_now=True)
+
+	class Meta:
+		ordering=['-created_on']
+
+	def __str__(self):
+		return str(self.name)
+
+class BookIssue(models.Model):
+	sudent=models.ForeignKey(Student,on_delete=models.CASCADE,null=True)
+	book=models.ForeignKey(Book,on_delete=models.CASCADE,null=True)
+	penality=models.IntegerField(default=0)
+	book_return=models.CharField(max_length=50,choices=RETURN_CHOICES,null=True,blank=True)
+	created_on=models.DateTimeField(auto_now_add=True)
+	updated_on=models.DateTimeField(auto_now=True)
+
+
+	class Meta:
+		ordering=['-created_on']
+
+	def __str__(self):
+		return str(self.student)
+
+class Event(models.Model):
+	event_name=models.CharField(max_length=100)
+	event_description=models.TextField()
+	created_on=models.DateTimeField(auto_now_add=True)
+	updated_on=models.DateTimeField(auto_now=True)
+
+	class Meta:
+		ordering=['-created_on']
+
+	def __str__(self):
+		return str(self.event_name)
+
+class Due(models.Model):
+	book_issue=models.ForeignKey(BookIssue,on_delete=models.CASCADE,null=True)
+	payment=models.IntegerField()
+	created_on=models.DateTimeField(auto_now_add=True)
+	updated_on=models.DateTimeField(auto_now=True)
+
+	class Meta:
+		ordering=['-created_on']
+
+	def __str__(self):
+		return str(self.book_issue)
+
+
+
+
 class Contact(models.Model):
 	email=models.EmailField(max_length=50)
 	user=models.ForeignKey(User,on_delete=models.CASCADE,null=True)
